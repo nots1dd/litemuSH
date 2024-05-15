e8f1ff84f229a1adef028ace4db2706518b87685
nots1dd <sid9.karanam@gmail.com>
2024-05-15 22:34:48 +0530

    feat: install script conf

--- install.sh
+++ install.sh
@@ -1,33 +1,55 @@
 #!/bin/bash
 
-REPO_URL="https://github.com/nots1dd/litemus" # in case you need this
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BOLD='\033[1m'
 
-INSTALL_DIR="~/litemus"
+REPO_URL="https://github.com/nots1dd/litemus"
+INSTALL_DIR="$HOME/litemus"
+SCRIPT_NAME="main.sh"
+
+# Function to install dependencies
+install_dependencies() {
+    echo "Installing dependencies..."
+    sudo pacman -S --noconfirm bc ffmpeg viu grep awk
+    echo "${GREEN}${BOLD}Dependencies installed."
+    echo -e "${YELLOW}In order to install smenu, you need to use yay or build from source.\nNote: Without this the script will NOT run!\n"
+}
+
+# Function to create alias for the music player
+create_alias() {
+    local shell_name="${SHELL##*/}"
+
+    if [ "$shell_name" == "bash" ]; then
+        echo "${YELLOW}alias lmus='$INSTALL_DIR/$SCRIPT_NAME'" >> "$HOME/.bashrc"
+        echo -e "${GREEN}Alias 'lmus' created for bash users. You can now use 'lmus' to run the music player."
+    elif [ "$shell_name" == "zsh" ]; then
+        echo "${YELLOW}alias lmus='$INSTALL_DIR/$SCRIPT_NAME'" >> "$HOME/.zshrc"
+        echo "${GREEN}Alias 'lmus' created for zsh users. You can now use 'lmus' to run the music player."
+    else
+        echo "${RED}Unsupported shell detected. Please manually add the alias for your shell."
+    fi
+}
+
+# # Check if the installation directory exists
+# if [ ! -d "$INSTALL_DIR" ]; then
+#     echo "Creating installation directory..."
+#     mkdir -p "$INSTALL_DIR"
+# fi
 
 # Check if the download was successful
 if [ $? -eq 0 ]; then
-
     # Make the script executable
-    chmod +x "$INSTALL_DIR/$SCRIPT_NAME"
 
-    # Check the user's shell and create the appropriate alias
-    if [ "${SHELL##*/}" == "bash" ]; then
-        # For bash users
-        echo "alias lmus=$INSTALL_DIR/main.sh" >> ~/.bashrc
-        echo "Alias 'lmus' created for bash users. You can now use 'lmus' to run the music player."
-    elif [ "${SHELL##*/}" == "zsh" ]; then
-        # For zsh users
-        echo "alias lmus=$INSTALL_DIR/main.sh" >> ~/.zshrc
-        echo "Alias 'lmus' created for zsh users. You can now use 'lmus' to run the music player."
-    else
-        echo "Unsupported shell detected. Please manually add the alias for your shell."
-    fi
+    # Install dependencies
+    install_dependencies
 
+    # Create alias for the music player
+    create_alias
 else
-    echo "Failed to download the script."
+    echo "${RED}${BOLD}Failed to download the script."
     exit 1
 fi
 
-
-# this is a very barebones script as there is only one executable file (main.sh)
-# in the future this will be updated accordingly
+echo -e "${GREEN}${BOLD}Installation complete."
