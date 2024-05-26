062e43143e286d2b66071dab7e27b01cc6aadaef
nots1dd <sid9.karanam@gmail.com>
2024-05-26 17:41:16 +0530

    chore: folder restruct

--- utils/modules.sh
+++ utils/modules.sh
@@ -1,11 +1,11 @@
 # all sources go here
 source $src/lyrics/zsh_lyrics.sh
 source $src/utils/directory.sh
-source $src/functions/extract_cover.sh
+source $src/functions/display/extract_cover.sh
 source $src/functions/playback/toggle_playback.sh
 source $src/functions/volume_ctrl.sh
-source $src/functions/display.sh
-source $src/functions/duration.sh
+source $src/functions/display/display.sh
+source $src/functions/display/duration.sh
 source $src/functions/get_lyrics.sh
 source $src/functions/load_songs.sh
 source $src/functions/playback/play_next.sh
@@ -17,5 +17,5 @@ source $src/functions/playback/monitor_pb.sh
 source $src/functions/playback/ffplay_index.sh # for ffplay
 source $src/functions/playback/time.sh
 source $src/functions/keybinds.sh
-source $src/functions/load_cache.sh
-source $src/functions/save_cache.sh
+source $src/functions/cache/load_cache.sh
+source $src/functions/cache/save_cache.sh
